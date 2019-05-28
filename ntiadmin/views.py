202085c9d0b04c820c230987bc283ef37a7ca708
KaEnfors <37064780+KaEnfors@users.noreply.github.com>
2019-05-28 18:05:09 +0200

    Added initial DB-structure, templates, stylesheets and some url+views

--- ntiadmin/views.py
+++ ntiadmin/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def tools(request):
@@ -8,6 +9,7 @@ def tools(request):
     HttpResponse(render(request, "Hello Tools!"))
 
 def ews(request):
+    response = loader.get_template('ntiadmin/ews/ews_table.html')
 
+    return HttpResponse(response.render())
 
-    return HttpResponse(render(request, "Hello EWS!"))
